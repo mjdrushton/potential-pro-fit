94fa34abd53df9471ef8953eab6307a98689e3dc
Michael Rushton <m.j.d.rushton@gmail.com>
2017-11-04 23:49:10 +0000

    Added vagrant config files that provision an ubuntu virtual box with slurm, which will be used for testing purposes.

---
+++ tests/vagrant/slurm/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+apt-get --yes install slurm-llnl
+cp /vagrant/slurm.conf /etc/slurm-llnl/slurm.conf
+/usr/sbin/create-munge-key
+/etc/init.d/slurm-llnl start
+/etc/init.d/munge start
